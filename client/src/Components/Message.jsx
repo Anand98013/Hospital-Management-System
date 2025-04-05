f88380c10aba4eeee5b869f10b9b54a2010e211f
Anand Kumar <111699706+Anand98013@users.noreply.github.com>
2025-04-06 01:41:56 +0530

    Update Message.jsx

--- client/src/Components/Message.jsx
+++ client/src/Components/Message.jsx
@@ -14,7 +14,7 @@ const Message = () => {
     try {
       await axios
         .post(
-          "http://localhost:8000/api/v1/message/send",
+          "https://hospital-management-system-oo6r.onrender.com/api/v1/message/send",
           { firstName, lastName, email, phone, message },
           {
             withCredentials: true,
