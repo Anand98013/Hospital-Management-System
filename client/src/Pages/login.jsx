95ce585e3c248593836da3be861c3513bc37ee09
Anand Kumar <111699706+Anand98013@users.noreply.github.com>
2025-04-06 01:47:07 +0530

    Update login.jsx

--- client/src/Pages/login.jsx
+++ client/src/Pages/login.jsx
@@ -52,7 +52,7 @@ const login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:8000/api/v1/users/login",
+        "https://hospital-management-system-oo6r.onrender.com/api/v1/users/login",
         { email, password, confirmPassword: password, role: "Patient" },
         {
           // headers: {
