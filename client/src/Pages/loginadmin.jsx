7c1f0cc5d0eda95fc27c98fdc62d711e2497121e
Anand Kumar <111699706+Anand98013@users.noreply.github.com>
2025-04-06 01:47:38 +0530

    Update loginadmin.jsx

--- client/src/Pages/loginadmin.jsx
+++ client/src/Pages/loginadmin.jsx
@@ -22,7 +22,7 @@ const loginadmin = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:8000/api/v1/users/login",
+        "https://hospital-management-system-oo6r.onrender.com/api/v1/users/login",
         { email, password, confirmPassword: password, role: "Admin" },
         {
           headers: { "Content-Type": "application/json" },
